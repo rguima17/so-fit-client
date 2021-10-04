e2231325826f6963860cfc75b3a1349e4f5956dd
Manoel Pereira <manoelpereiraeng@gmail.com>
2021-10-04 09:50:59 -0300

    minor improvements to routes and removal of bootstrap

--- src/components/userFeed/UserFeed.js
+++ src/components/userFeed/UserFeed.js
@@ -4,19 +4,15 @@ import UserPosts from "../posting/UserPosts";
 import FollowingPosts from "../posting/FollowingPosts";
 
 function UserFeed() {
-   
-   return (
-       <div>
-          <h2> User Feed</h2>
-          <AllUsers />
-          < UserFollowing />
-         <UserPosts/>
-         <FollowingPosts />
-
-
-           </div>
-   )
+  return (
+    <div>
+      <h2> User Feed</h2>
+      <AllUsers />
+      <UserFollowing />
+      <UserPosts />
+      <FollowingPosts />
+    </div>
+  );
 }
 
-export default UserFeed
-
+export default UserFeed;
