e2fc32b3feb5e33fb28c262195269006a7644179
Raul Guimaraes <raul.metalurgica134@gmail.com>
2021-10-07 21:17:58 -0300

    fixing navbar

--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -8,7 +8,7 @@ import imgBiking from "../assets/img/slides/Bike.png";
 
 function Home() {
   return (
-    <div className="relative bg-white overflow-hidden ">
+    <div className="relative bg-white overflow-hidden z-0 ">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
